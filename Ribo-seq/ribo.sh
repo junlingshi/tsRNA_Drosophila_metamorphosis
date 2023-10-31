da4b2a200933e2b3e2f3eafc6e99b1b5523de07e
junlingshi <149343075+junlingshi@users.noreply.github.com>
2023-10-31 13:56:23 +0800

    Update

--- Ribo-seq/ribo.sh
+++ Ribo-seq/ribo.sh
@@ -6,6 +6,8 @@ cat *_trans.sam|grep ^A|awk '{if($6!="*")print $0}'|sed 's/XG:i:1/XG:i:0/g'|sed 
 cat aa|cut -f 3|sed 's/M/\t/g'|awk '{sum=0};{for (i=1;i<=NF;i++) {sum+=$i};print sum}'> cc
 paste aa cc|awk '{print $1"\t"$2"\t"$4}' > Scr-1_trans.txt
 join *_trans.txt /storage/SJL/reference/dmel/length_max.txt|awk '{if($2>($5+15)&& $2<($8-$7-15))print $1}'|uniq -c|awk '{print $2"\t"$1}' > Scr-1_counts.txt
+###Scr-1_trans.txt, First column: FlyBase Geneid; Second column: start position; third column: length; 
+###length_max.txt, First column: FlyBase Geneid; Second column: the id of transcript with longest CDS; Third column: the length of 5UTR; Fourth column: the length of CDS; Fifth column: the length of 3UTR; Sixth column: the length of transcript;  
 
 ### Determination of A site
 join ~/ribo/20220607/Scr-1/Scr-1_trans.txt /storage/SJL/reference/dmel/length_max.txt|awk '{if($2>($5+15)&& $2<($8-$7-15))print $0}'|awk '{if($3>=30 && $3<=32)print $0}'|awk '{print $1"\t"$2-$5"\t"$3}'|awk '{print $1"\t"$2"\t"$2 % 3}'|awk '{if($3=="1") print $1"\t"$2+15"\t"$3}'|awk '{if($2>=0)print $1"\t"$2-1"\t"$2+2}' > Scr-1.bed
@@ -13,4 +15,3 @@ bedtools getfasta -fi /storage/SJL/ribo/20220607/Scr-1/test.fasta -bed Scr-1.bed
 cat Scr-1.bed |awk '{print $1"_"$2"-"$3}'|sort -u >aa
 cat Scr-1.fasta |awk '/^>/&&NR>1{print ""};{printf "%s",/^>/ ? $0" ":$0}'|sed 's/>//g'|sed 's/:/_/g'|sort -k1,1 >bb
 join aa bb| sed 's/_/\t/g'|sed 's/-/\t/g'|awk '{print $4}'|sort |uniq -c|awk '{print $2"\t"$1}' > Scr-1_A.txt
-
